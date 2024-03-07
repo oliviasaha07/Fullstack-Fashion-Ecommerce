3de3b93549dc2721aa152f5796eed5b26bff1143
Olivia Saha <107919663+oliviasaha07@users.noreply.github.com>
2024-03-07 22:28:24 +0530

    Create home_controller.js

---
+++ controllers/controllers/home_controller.js
@@ -0,0 +1,9 @@
+
+
+module.exports.home = function (request, response) {
+  console.log("Home controller action is working");
+  
+  return response.render("home", {
+    title: "Home View",
+  });
+};
